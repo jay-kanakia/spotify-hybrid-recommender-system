38ec8d72713de234573f61e6106c4fd472b66818
Jay Kanakia <jay.kanakia26@gmail.com>
2026-04-13 12:18:47 +0530

    EDA on spotify dataset

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A Spotify hybrid recommender system using weightage combination of content based filtering and collabrative type filtering. A end-to-end MLOPs project from data cleaning, EDA to deploying on AWS',
+    author='Jay Kanakia',
+    license='',
+)
